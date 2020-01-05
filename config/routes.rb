01ad2f39cab6ef8f6f208c05498c1b3b85c6054a
fdolzanes1 <fdolzanes1@gmail.com>
2020-01-05 15:33:07 -0400

    update page index and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'exchanges/index'
-
-  get 'exchanges/exchange'
+  get '/', to: 'exchanges#index'
+  get 'exchange', to: 'exchanges#exchange'
   get 'convert', to: 'exchanges#convert'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
